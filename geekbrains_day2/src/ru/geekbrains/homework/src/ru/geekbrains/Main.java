993f3f297bf567a726303cf89afa657f23940a02
sol <solo-yes@mail.ru>
2017-10-02 20:07:12 +0300

    test

--- geekbrains_day2/src/ru/geekbrains/homework/src/ru/geekbrains/Main.java
+++ geekbrains_day2/src/ru/geekbrains/homework/src/ru/geekbrains/Main.java
@@ -10,11 +10,30 @@ public class Main {
         method3();
         method4(5);
         method5();
-        System.out.println(method6(new int[]{1, 1, 1, 2, 1}));
-        System.out.println(method6(new int[]{2, 1, 1, 2, 1}));
-        System.out.println(method6(new int[]{10, 10}));
-        System.out.println(method6(new int[]{1, 2, 3, 4, 10}));
-        System.out.println(method6(new int[]{1, 2}));
+        //method6:
+        System.out.println(method6(new int[]{1, 1, 1, 2, 1})); //true
+        System.out.println(method6(new int[]{2, 1, 1, 2, 1})); //false
+        System.out.println(method6(new int[]{10, 10})); //true
+        System.out.println(method6(new int[]{1, 2, 3, 4, 10})); //true
+        System.out.println(method6(new int[]{1, 2})); //false
+        System.out.println(method6(new int[]{1})); //true
+        //
+
+        //method7
+        System.out.println(Arrays.toString(method7(new int[]{1, 2, 3, 4, 5}, 1)));
+        System.out.println();
+        System.out.println(Arrays.toString(method7(new int[]{1, 2, 3, 4, 5}, -1)));
+        System.out.println();
+        System.out.println(Arrays.toString(method7(new int[]{1}, -1)));
+        System.out.println();
+        System.out.println(Arrays.toString(method7(new int[]{1, 2, 3, 4, 5}, 0)));
+        //
+
+        //Extra methods
+        System.out.println(Arrays.toString(method8(new int[]{0,0,0,0,1,1,1,1,1,1,1,0,0,1,1,1,1})));
+        System.out.println(Arrays.toString(method8(new int[]{0,0,0,0,1,1,1,1,1,1,1,1,1})));
+        System.out.println(Arrays.toString(method8(new int[]{0,0,0,1,1,1,1,1,0,1,1,1})));
+        //
     }
 
     private static void out(int[] array){
@@ -88,6 +107,73 @@ public class Main {
      }
 
      public static boolean method6(int[] array) {
-     
+         if (array.length == 1) return true;
+         for (int j = 0; j < array.length; j ++) {
+             int tmp1 = 0;
+             int tmp2 = 0;
+             for (int i = 0; i < array.length; i++) {
+                 if (i <= j) tmp1 += array[i];
+                 if (i > j) tmp2 += array[i];
+             }
+             if (tmp1 == tmp2) return true;
+         }
+     return false;
+     }
+
+     public static int[] method7 (int[] array, int n){
+         System.out.println(Arrays.toString(array));
+         if (n == 0 || array.length == 1) return array;
+         if (n > 0) {
+             for (int i = 0; i < n; i++) {
+                 int last = array[array.length - 1];
+                 for (int j = array.length - 1; j > 0; j--) {
+                     array[j] = array[j - 1];
+                 }
+                 array[0] = last;
+             }
+         }
+         if (n < 0){
+             n = -n;
+             for (int i = 0; i < n; i++) {
+                 int first = array[0];
+                 for (int j = 0; j < array.length - 1; j++) {
+                     array[j] = array[j + 1];
+                 }
+                 array[array.length - 1] = first;
+             }
+         }
+         return array;
+
+     }
+     public static int[] method8(int[] array){
+         //Without ArrayList
+         System.out.println(Arrays.toString(array));
+         int[] out = new int[array.length];
+         int tmp = 1;
+         int j = 0;
+         for (int i = 0; i < array.length - 1; i++) {
+             if (array[i] == array[i + 1]) {
+                 tmp ++;
+             }
+             else {
+                 out[j] = tmp;
+                 tmp = 1;
+                 j ++;
+             }
+         }
+         out[j] = tmp;
+
+         int tmp1 = 0;
+         for (int i = 0; i < out.length - 1; i++) {
+             if(out[i] == 0) break;
+             tmp1 ++;
+         }
+
+         int[] out1 = new int[tmp1];
+
+         for (int i = 0; i < tmp1; i++) {
+             out1[i] = out[i];
+         }
+         return out1;
      }
 }
