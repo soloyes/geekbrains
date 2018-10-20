99d3790d29fafe3383630966ea3f426e33e4a1a1
sol <solo-yes@mail.ru>
2018-10-20 13:30:48 +0300

    Task5

---
+++ geekspring_thymeleaf/src/main/java/com/geekbrains/services/CoursesService.java
@@ -0,0 +1,28 @@
+package com.geekbrains.services;
+
+import com.geekbrains.entities.Course;
+import com.geekbrains.entities.Student;
+import com.geekbrains.repositories.CoursesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+public class CoursesService {
+    private CoursesRepository coursesRepository;
+
+    @Autowired
+    public void setCoursesRepository(CoursesRepository coursesRepository) {
+        this.coursesRepository = coursesRepository;
+    }
+
+    public CoursesService() {
+    }
+
+    @Transactional
+    public List<Course> getAllCoursesList() {
+        return (List<Course>) coursesRepository.findAll();
+    }
+}
---
+++ geekspring_thymeleaf/src/main/webapp/resources/js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    $('.removeBtn').on('click', function (event) {
+        var studentId = $(this).attr('entryIndex');
+        $.get("/students/remove/" + studentId);
+        location.reload();
+    });
+
+    $('#consoleTestBtn').on('click', function (event) {
+        console.log($('#myInput').val());
+    });
+
+    $('#student_courses tr').click(function () {
+        var course_id = $(this).find('td').eq(0).text();
+        var student_id = window.location.pathname.split("/")[2];
+        $.get("/students/deleteCourse/" + student_id + "/" + course_id);
+        //location.reload();
+    });
+
+    $('#all_courses tr').click(function () {
+        var course_id = $(this).find('td').eq(0).text();
+        var student_id = window.location.pathname.split("/")[2];
+        $.get("/students/addCourse/" + student_id + "/" + course_id);
+        //location.reload();
+    });
+});
